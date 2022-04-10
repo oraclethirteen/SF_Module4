f4c7ec47804511dfa0816fd67a0656fd6a66b884
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-04-11 00:52:29 +0300

    Add project files.

---
+++ SF_Module4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SF_Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите название вашего любимого цвета на английском (с маленькой буквы)");
+
+            var color = Console.ReadLine();
+
+            if(color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+            else if(color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+        }
+    }
+}
